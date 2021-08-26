8160c703979c72befd95a47cfe7e792679b4b1ba
G Sudarshan <43257931+G-Sudarshan@users.noreply.github.com>
2021-08-26 22:37:19 +0530

    Create Find the Highest Altitude.java

---
+++ Array/Easy/Find the Highest Altitude.java
@@ -0,0 +1,56 @@
+/*
+
+1732. Find the Highest Altitude
+Easy
+
+435
+
+46
+
+Add to List
+
+Share
+There is a biker going on a road trip. The road trip consists of n + 1 points at different altitudes. The biker starts his trip on point 0 with altitude equal 0.
+
+You are given an integer array gain of length n where gain[i] is the net gain in altitude between points i​​​​​​ and i + 1 for all (0 <= i < n). Return the highest altitude of a point.
+
+ 
+
+Example 1:
+
+Input: gain = [-5,1,5,0,-7]
+Output: 1
+Explanation: The altitudes are [0,-5,-4,1,1,-6]. The highest is 1.
+Example 2:
+
+Input: gain = [-4,-3,-2,-1,4,3,2]
+Output: 0
+Explanation: The altitudes are [0,-4,-7,-9,-10,-6,-3,-1]. The highest is 0.
+ 
+
+Constraints:
+
+n == gain.length
+1 <= n <= 100
+-100 <= gain[i] <= 100
+Accepted
+46,921
+Submissions
+59,242
+
+*/
+
+class Solution {
+    public int largestAltitude(int[] gain) {
+        int max_altitude = 0;
+        int current_altitude=0;
+        for(int alt: gain){
+            current_altitude = current_altitude + alt;
+            if(current_altitude>max_altitude){
+                max_altitude = current_altitude;
+            }
+        }
+        
+        return max_altitude;
+    }
+}
