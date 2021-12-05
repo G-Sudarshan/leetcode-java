7c3ebf6a4eaa281fa583e01c9ee0b7cbf732d17f
G Sudarshan <43257931+G-Sudarshan@users.noreply.github.com>
2021-12-05 16:14:40 +0530

    Create Longest Common Subsequence.java

---
+++ Dynamic Programming/Medium/Longest Common Subsequence.java
@@ -0,0 +1,56 @@
+/*
+Longest Common Subsequence 
+Medium Accuracy: 49.98% Submissions: 62776 Points: 4
+Given two sequences, find the length of longest subsequence present in both of them. Both the strings are of uppercase.
+
+Example 1:
+
+Input:
+A = 6, B = 6
+str1 = ABCDGH
+str2 = AEDFHR
+Output: 3
+Explanation: LCS for input Sequences
+“ABCDGH” and “AEDFHR” is “ADH” of
+length 3.
+Example 2:
+
+Input:
+A = 3, B = 2
+str1 = ABC
+str2 = AC
+Output: 2
+Explanation: LCS of "ABC" and "AC" is
+"AC" of length 2.
+Your Task:
+Complete the function lcs() which takes the length of two strings respectively and two strings as input parameters and returns the length of the longest subsequence present in both of them.
+
+Expected Time Complexity : O(|str1|*|str2|)
+Expected Auxiliary Space: O(|str1|*|str2|)
+
+Constraints:
+1<=size(str1),size(str2)<=103
+*/
+
+class Solution
+{
+    //Function to find the length of longest common subsequence in two strings.
+    static int lcs(int x, int y, String s1, String s2)
+    {
+        // your code here
+        int[][] t = new int[x+1][y+1];
+        
+        for(int i=1; i<x+1; i++){
+            for(int j=1; j<y+1; j++){
+                if(s1.charAt(i-1)==s2.charAt(j-1)){
+                    t[i][j] = 1 + t[i-1][j-1];
+                }else{
+                    t[i][j] = Math.max(t[i-1][j],t[i][j-1]);
+                }
+            }
+        }
+        
+        return t[x][y];
+    }
+    
+}
