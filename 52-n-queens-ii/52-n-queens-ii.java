dc927e5ac57c47f34b11b46ee86e812092d69430
Sudarshan Gawale <43257931+G-Sudarshan@users.noreply.github.com>
2022-05-12 12:03:52 +0530

    Time: 2 ms (82.28%), Space: 38.9 MB (93.93%) - LeetHub

---
+++ 52-n-queens-ii/52-n-queens-ii.java
@@ -0,0 +1,48 @@
+class Solution {
+      int count = 0;
+      public int totalNQueens(int n) {
+            char[][] board = new char[n][n];
+            for(int i=0; i<n; i++){
+                for(int j=0; j<n; j++){
+                    board[i][j] = '.';
+                }
+            }       
+            solve(board, 0, n);
+            return count;
+         }
+    
+    public void solve(char[][] board, int row, int n){
+        if(row==n){
+           count++;
+           return;
+        }
+        
+        for(int col=0; col<n; col++){
+            if(isValid(board, row, col)){
+                board[row][col] = 'Q';
+                solve(board, row+1, n);
+                board[row][col] = '.';
+            }
+        }
+        
+    }
+    
+    public boolean isValid(char[][] board, int row, int col){
+        for(int i=row-1; i>=0; i--){
+            if(board[i][col] == 'Q')
+                return false;
+        }
+        
+        for(int i= row-1, j=col-1; i>=0 && j>=0; i--, j--){
+            if(board[i][j] == 'Q')
+                return false;
+        }
+        
+        for(int i=row-1, j=col+1; i>=0 && j<board.length; i--, j++){
+            if(board[i][j] == 'Q')
+                return false;
+        }
+        return true;
+    }
+    
+}
