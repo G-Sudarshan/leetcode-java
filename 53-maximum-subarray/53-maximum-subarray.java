67953fac4aa374655bff9fc310b6082e61d9f313
Sudarshan Gawale <43257931+G-Sudarshan@users.noreply.github.com>
2022-10-16 16:48:45 +0530

    Time: 2 ms (78.15%), Space: 81.3 MB (14.86%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.java
+++ 53-maximum-subarray/53-maximum-subarray.java
@@ -3,9 +3,10 @@ class Solution {
         int cur_max = 0;
         int max = Integer.MIN_VALUE;
         
-        for(int n: nums){
-            cur_max = Math.max(n, cur_max+n);
-            max = Math.max(max, cur_max);
+        for(int i=0; i<nums.length; i++){
+            cur_max = Math.max(nums[i], cur_max + nums[i]);
+            if(cur_max>max)
+                max = cur_max;
         }
         
         return max;
